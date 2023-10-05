146936bf4e71c1b6f701d471295726e4f9078c28
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-05 09:53:26 -0300

    add: UsersController

--- src/database/knex/migrations/20231005114535_favorites.js
+++ src/database/knex/migrations/20231005114535_favorites.js
@@ -1,8 +1,8 @@
 exports.up = knex => knex.schema.createTable("favorites", table => {
     table.increments("id").primary()
     table.text("name").unique()
-    table.integer("dishe_id").references("id").inTable("dishes")
-    table.integer("user_id").references("id").inTable("users")
+    table.integer("dishe_id").references("id").inTable("dishes").onDelete("CASCADE")
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE")
 })
 
 exports.down = knex => knex.schema.dropTable("favorites")
