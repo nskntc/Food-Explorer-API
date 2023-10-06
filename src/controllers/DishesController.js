cfc90879874f9a0d6ff1c4eb76b03f107db52246
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-06 18:33:58 -0300

    add: show and index methods to DishesController

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -8,8 +8,6 @@ class DishesController {
 
         const fileName = null
 
-        console.log(name, description, price, category, ingredients)
-
         if(!name || !description || !price || !category || !ingredients){
             throw new AppError("Por favor, preencher todos os campos!", 422)
         }
@@ -41,6 +39,101 @@ class DishesController {
         await knex("ingredients").insert(ingredientsInsert)
 
         return response.status(201).json()
+    }
+
+    async update(request, response){
+        const { id } = request.params
+        const { name, description, price, category, ingredients } = request.body
+
+        if(!name || !description || !price || !category || !ingredients){
+            throw new AppError("Por favor, preencher todos os campos!", 422)
+        }
+
+        const dish = await knex("dishes").where({ id }).first()
+        if(!dish) throw new AppError("Prato não cadastrado!", 404)
+
+        const updatedDish = {
+            name,
+            description,
+            price,
+            category,
+            updated_at: knex.fn.now()
+        }
+
+        await knex("dishes")
+        .where({ id })
+        .update(updatedDish)
+
+        if(ingredients){
+            const updatedIngredients = ingredients.map(ingredient => {
+                return{
+                    dish_id: id,
+                    name: ingredient
+                }
+            })
+
+            await knex("ingredients").where({ dish_id: id }).delete();
+            await knex('ingredients').insert(updatedIngredients);
+        }
+
+        return response.status(201).json()
+    }
+
+    async show(request, response){
+        const { id } = request.params
+
+        const dish = await knex("dishes").where({ id }).first()
+        if(!dish) throw new AppError("Prato não cadastrado!", 404)
+
+        const ingredients = await knex("ingredients").where({ dish_id: id }).orderBy("name")
+
+        return response.status(201).json({
+            ...dish,
+            ingredients
+        })
+    }
+
+    async index(request, response){
+        const { name, ingredients } = request.query
+        console.log(name, ingredients)
+
+        let dishes
+
+        if (ingredients) {
+            const filterIngredients = ingredients.split(',').map(ingredient => ingredient.trim())
+
+            dishes = await knex("ingredients")
+            .select([
+                "dishes.id",
+                "dishes.name",
+                "dishes.description",
+                "dishes.price",
+                "dishes.category",
+                "dishes.img"
+            ])
+            .whereLike("dishes.name", `%${name}%`)
+            .whereIn("ingredients.name", filterIngredients)
+            .innerJoin("dishes", "dishes.id", "ingredients.dish_id")
+            .groupBy("dishes.id")
+            .orderBy("dishes.name")
+        } else{
+            dishes = await knex("dishes")
+            .whereLike("name", `%${name}%`)
+            .orderBy("name")
+        }
+        
+        const ingredientsTable = await knex("ingredients")
+
+        const DisheWithIngredients = dishes.map(dish => {
+            const dishIngredients = ingredientsTable.filter(ingredient => ingredient.dish_id === dish.id)
+
+            return{
+                ...dish,
+                ingredients: dishIngredients
+            }
+        })
+
+        return response.status(201).json(DisheWithIngredients)
     }
 }
 
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -6,5 +6,8 @@ const dishesRoutes = Router()
 const dishesController = new DishesController()
 
 dishesRoutes.post("/", dishesController.create)
+dishesRoutes.put("/:id", dishesController.update)
+dishesRoutes.get("/:id", dishesController.show)
+dishesRoutes.get("/", dishesController.index)
 
 module.exports = dishesRoutes
