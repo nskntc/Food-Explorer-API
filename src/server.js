46c79d507f1fbce0e5cd3e84c6c94474c54efa61
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-11 11:33:30 -0300

    add: dotenv

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors")
 
 const routes = require("./routes")
@@ -30,5 +31,5 @@ app.use((error, request, response, next) => {
         })
     })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
